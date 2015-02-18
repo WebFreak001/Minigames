167ac5da2ac198d10834a0dfb1df5733074b6ac4
WebFreak001 <janju007@web.de>
2015-02-18 22:01:16 +0100

    Actually removed .class files

--- src/com/naronco/minigames/Main.java
+++ src/com/naronco/minigames/Main.java
@@ -11,30 +11,27 @@ import com.naronco.minigames.stomp.StompClient;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Vector<IGame> games = new Vector<IGame>();
-		games.add(new Pong());
-		games.add(new Snake());
+	    Vector<IGame> games = new Vector<IGame>();
+        games.add(new Pong());
+        games.add(new Snake());
         games.add(new Stomp());
         games.add(new StompClient());
-		int i = 0;
+        
+        Object[] gameNames = { "Pong", "Snake", "Stomp", "Stomp (Join Server)" };
+        int i = -1;
+        
+        if (gameNames.length != 1) {
+            String s = (String) JOptionPane.showInputDialog(null,
+                    "Choose from one of those games: ", "Choose a Game",
+                    JOptionPane.PLAIN_MESSAGE, null, gameNames, "Pong");
 
-		Object[] gameNames = { "Pong", "Snake", "Stomp", "Stomp (Join Server)" };
-		if (gameNames.length != 1) {
-			String s = (String) JOptionPane.showInputDialog(null,
-					"Choose from one of those games: ", "Choose a Game",
-					JOptionPane.PLAIN_MESSAGE, null, gameNames, "Pong");
-
-			if (s.equalsIgnoreCase(gameNames[0].toString()))
-				i = 0;
-			else if (s.equalsIgnoreCase(gameNames[1].toString()))
-				i = 1;
-            else if (s.equalsIgnoreCase(gameNames[2].toString()))
-                i = 2;
-            else if (s.equalsIgnoreCase(gameNames[3].toString()))
-                i = 3;
-			else
-				throw new Exception(s);
-		}
+            for(int ii = 0; ii < gameNames.length; ii++) {
+                if(s.equalsIgnoreCase(gameNames[ii].toString()))
+                    i = ii;
+            }
+            if(i == -1)
+                throw new Exception(s);
+        }
 
 		new Game(games.get(i)).start();
 	}
